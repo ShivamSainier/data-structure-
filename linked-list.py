820e98ac4bca6dabc976bc3e4e8d068dbe6aa070
unknown <ss1907364@gmail.com>
2020-02-02 11:09:56 +0530

    my first linked-list

--- linked-list.py
+++ linked-list.py
@@ -1,16 +1,9 @@
-# A linked list is represented by a pointer to the first node of the linked list. The first node is called the head. If the linked list is empty, then the value of the head is NULL.
-# Each node in a list consists of at least two parts:
-# 1) data
-# 2) Pointer (Or Reference) to the next node
-
-# Let us create a simple linked list with 3 nodes.
 class node:
     def __init__(self,data):
         self.data=data
         self.next=None
-    
-    
-class linkedlistt:
+
+class linkedlist:
     def __init__(self):
         self.head=None
     def printlist(self):
@@ -18,12 +11,10 @@ class linkedlistt:
         while temp:
             print(temp.data)
             temp=temp.next
-
-
-n1=node("first-data")
-n2=node("second-data")
-n3=node("third-data")
-listt=linkedlistt()
+n1=node("node-1")
+n2=node("node-2")
+n3=node("node-3")
+listt=linkedlist()
 listt.head=n1
 n1.next=n2
 n2.next=n3
